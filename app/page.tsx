dacdbf1120fd0c1d941d9278e82cafdfeeafce9c
Arpan Gupta <arpan74@gmail.com>
2025-05-20 08:43:21 -0500

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen p-8 flex flex-col items-center justify-center font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 items-center max-w-3xl w-full">
+        
+      </main>
+    </div>
+  );
+}
